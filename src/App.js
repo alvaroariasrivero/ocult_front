fd0af6785e8d76f64b1bcc8507da937660754606
Álvaro Arias Rivero <alvaroariasrivero92@gmail.com>
2022-01-14 15:05:23 +0100

    creados componentes y fetch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
+import React, {useState, useEffect } from "react";
+import { BrowserRouter } from 'react-router-dom';
+import Main from './components/Main';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const[questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    async function fetchQuestion() {
+      try {
+        const res = await axios.get('https://opentdb.com/api.php?amount=10&category=11&type=multiple');
+        const json = res.data;
+        const array = json.results
+        const questionsArray = array.map(element => {
+          return{
+            'question': element.question,
+            'answers': [
+              element.correct_answer,
+              element.incorrect_answer[0],
+              element.incorrect_answer[1],
+              element.incorrect_answer[2]
+            ]
+          };
+        })
+        setQuestions(questionsArray)
+      } catch (error){
+        console.log('error', error);
+      }
+    };
+    fetchQuestion();
+  })
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Header/>
+        <Main/>
+        <Footer/>
+      </BrowserRouter>
     </div>
   );
 }
