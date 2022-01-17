7ab3fb618e75541aac6913fe3c949655b76f475f
IsaGuapo <isabela.guapo@gmail.com>
2022-01-17 21:55:18 +0100

    add flex-box and basic funcionality to chatbot

--- src/components/Chatbot/Chatbot.jsx
+++ src/components/Chatbot/Chatbot.jsx
@@ -1,17 +1,41 @@
 import React, { Component } from "react";
 import ChatBot from 'react-simple-chatbot';
+import { NavLink } from 'react-router-dom';
+import './Chatbot.css'
 
 class Chatbot extends Component {
   render() {
-    return <ChatBot
-    steps={[
-      {
-        id: 'hello-world',
-        message: '¿En qué puedo ayudarte?',
-        end: true,
-      },
-    ]}
+    return <div className="chatbotContainer">
+      <aside className="asideNav">
+      <ul className="aside">
+        <li><img src="../assets/icons/aside_dashboard.png" alt="icon" /><NavLink to='/profile'>Inicio</NavLink></li>
+        <li><img src="../assets/icons/aside_support.png" alt="icon" /><NavLink to='/placeholder'>Recursos</NavLink></li>
+        <li><img src="../assets/icons/aside_notification.png" alt="icon" /><NavLink to='/placeholder'>Chatbot</NavLink></li>
+        <li><img src="../assets/icons/aside_notification.png" alt="icon" /><NavLink to='/placeholder'>Configuración</NavLink></li>
+        <li><img src="../assets/icons/aside_notification.png" alt="icon" /><NavLink to='/placeholder'>Ayuda</NavLink></li>
+      </ul>
+    </aside>
+    <div className="div_chatbot"></div>
+      <ChatBot
+          steps={[
+            {
+              id: '1',
+              message: 'Mi nombre es Oculty ¿En qué puedo ayudarte?',
+              trigger: '2',
+            },
+            {
+              id: '2',
+              user: true,
+              trigger: '3',
+            },
+            {
+              id: '3',
+              message: 'Voy a buscar toda la información necesaria para poder ayudarte. Dame un segundo!',
+              end: true,
+            },
+          ]}
   />;
+      </div>
   }
 }
 
