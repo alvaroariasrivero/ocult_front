4dd38e6fec99efd5c09385577257db9756ed3443
IsaGuapo <isabela.guapo@gmail.com>
2022-01-19 19:57:00 +0100

    add style at Prequiz

--- src/components/Prequiz/Prequiz.jsx
+++ src/components/Prequiz/Prequiz.jsx
@@ -12,7 +12,7 @@ const Prequiz = () =>{
 
 
   return <div className='div_container_prequiz'>
-    <div className='div_text_prequiz'>
+    <div className='div_welcome_prequiz'>
       <p>Hola {name}</p> 
       <p>vas a comenzar un test.</p>
       <p>Antes de empezar, recuerda:</p>
@@ -21,17 +21,21 @@ const Prequiz = () =>{
     <div className='div_text_prequiz'>
       <img className='img_prequiz' src='../assets/icons/Question.png'/>
       <p>¡No olvides responder a todas las preguntas! </p> 
+    </div>
+    <div className='div_text_prequiz'>
       <img className='img_prequiz' src='../assets/icons/oneway.png'/>
       <p>Lamentablemente, no podrás vover a atrás.</p>
-      <img className='img_prequiz' src='../assets/icons/thumbs.png'/>oneway
+    </div>
+    <div className='div_text_prequiz'>
+      <img className='img_prequiz' src='../assets/icons/thumbs.png'/>
       <p>En cada pregunta, echa un buen ojo al feedback.</p>
     </div>
 
-    <div className='div_imag_prequiz'>
+    {/* <div className='div_imag_prequiz'>
       <img src='../assets/icons/candadobackground.png'/>
-    </div>
-    <button className='btn_prequiz1'> <NavLink to="/quiz">Accede al test</NavLink> </button>
-    <button className='btn_prequiz2'><NavLink to="/profile">Dejar para más tarde</NavLink></button>
+    </div> */}
+    <button className='btn_prequiz'> <NavLink to="/quiz">Accede al test</NavLink> </button>
+    <button className='btn_prequiz'><NavLink to="/profile">Dejar para más tarde</NavLink></button>
 
   </div>
 
