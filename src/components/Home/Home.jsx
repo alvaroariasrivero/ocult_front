dae0a8bdefff296ad33fa0a9e0eb755f0060d1fc
IsaGuapo <isabela.guapo@gmail.com>
2022-01-20 16:16:04 +0100

    change size

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import './Home.css'
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 const Home = () => {
-  return <div>
-    <h1 className="title">¿Estás seguro de que esta página es segura? </h1>
-    <h3 className="title">Se nos ha olvidado preguntarte, ¿quién eres?</h3>
+  return <div className="container_home">
+    <div className="div_title1">
+      <h1 className="title">¿Estás seguro de que esta página es segura? </h1>
+    </div>
+    <div className="div_title2">
+      <h3 className="title">Se nos ha olvidado preguntarte, ¿quién eres?</h3>
+    </div>
+
     <Link to="/login" className="primary">Soy Empleado</Link>
     <Link to="/loginadmin" className="secondary">Soy responsable</Link>
   </div>;
